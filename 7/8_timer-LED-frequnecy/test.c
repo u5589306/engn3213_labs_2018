391bc5086c1a5a5411e9f2b4f981e652117d2347
u5589306 <u5589306@anu.edu.au>
2018-05-06 01:20:31 +1000

    rounding edges

--- 7/8_timer-LED-frequnecy/test.c
+++ 7/8_timer-LED-frequnecy/test.c
@@ -2,11 +2,11 @@
  * 7_uart_interrupt_tx_buffered.c
  *
  *	Objective:  Understand the UART (universal asynchronous receiver and transmitter)
- *				We will use the UART to echo back a received byte using UDRE interrupt. 
+ *				We will use the UART to echo back a received byte using UDRE interrupt.
  *
  * Created: 2/9/2018 9:32:26 PM
  * Author : Jon Kim
- */ 
+ */
 
 #define F_CPU 20000000
 
@@ -44,7 +44,7 @@ void put_str(char* str)
 unsigned char get_char(void)
 {
 	while ( !( UCSR0A & (1<<RXC0)) );
-	return UDR0;				
+	return UDR0;
 }
 
 void get_str(char* str)
@@ -66,7 +66,7 @@ int main(void)
 	OCR1AH = OCR1_VALUE_10HZ>>8;	// Write high byte first
 	OCR1AL = OCR1_VALUE_10HZ;
 	TIMSK1 = (1<<OCIE0A);
-	
+
 	UBRR0H = (unsigned char)(BAUD_VALUE>>8);
 	UBRR0L = (unsigned char)(BAUD_VALUE);
 	UCSR0C = (3<<UCSZ00);
--- 7/uart_interrrupt_rx_str/test.c
+++ 7/uart_interrrupt_rx_str/test.c
@@ -1,13 +1,3 @@
-/*
- * 7_uart_interrupt_tx_buffered.c
- *
- *	Objective:  Understand the UART (universal asynchronous receiver and transmitter)
- *				We will use the UART to echo back a received byte using UDRE interrupt.
- *
- * Created: 2/9/2018 9:32:26 PM
- * Author : Jon Kim
- */
-
 #define F_CPU	20000000
 
 #define BAUD_RATE	9600
@@ -17,7 +7,6 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
-#include <string.h>
 
 #define BUF_SIZE	128
 
@@ -26,71 +15,85 @@ int tx_buf_head = 0;
 volatile int tx_buf_tail = 0;
 volatile int tx_data_size = 0;
 
-unsigned char in_buffer[BUF_SIZE];
-int in_buf_head;
-volatile int in_buf_tail = 0;
-volatile int in_data_size = 0;
+unsigned char rx_data[128];
+int rx_buf_head = 0;
+int rx_data_size = 0;
+volatile int flag = 0;
+
 
 ISR(USART_UDRE_vect)
 {
 	/*
-		[TASK 2] Implement the ISR code here
-
+		[TASK 2] Implement the ISR code here 
+		
 		1. Check first whether or not there are data in the buffer.
-		2. If yes, write a byte to the UDR register. Make sure the tail index
-		   increments and rolls over to zero when it reaches the max buf size.
-		3. If not, ISR was invoked from the last buf data. So disable the UDRE interrupt.
+		2. If yes, write a byte to the UDR register. Make sure the tail index 
+		   increments and rolls over to zero when it reaches the max buf size.	 
+		3. If not, ISR was invoked from the last buf data. So disable the UDRE interrupt. 
 	*/
+
+	// flush data out
 	if (tx_data_size > 0){
 		UDR0 = tx_buf[tx_buf_tail];
-		tx_buf_tail = tx_buf_tail + 1;
+		tx_buf_tail++;
 		if (tx_buf_tail >= BUF_SIZE){
 			tx_buf_tail = 0;
 		}
 		tx_data_size--;
-	}
-	else{
-		unsigned char ch = UDR0;
-		UDR0 = ch;
+	} else {  // reset tx buffer
+		tx_data_size = 0;
+		tx_buf_tail = 0;
+		tx_buf_head = 0;
+		UCSR0B &= ~(1<<UDRIE0);
 	}
 }
-;
 
-void put_char_buf(char ch)
+
+ISR(USART_RX_vect){
+	unsigned char ch;
+	if ((ch = UDR0) != '\r'){  // read and save values to rx_data until \r
+		rx_data[rx_buf_head] = ch;
+		rx_buf_head++;
+		if (rx_buf_head >= BUF_SIZE){
+			rx_buf_head = 0;
+		}
+		rx_data_size++;
+	} else {
+		flag = 1;
+	}
+}
+
+void put_char_buf(unsigned char ch)
 {
 	/*
-		[TASK 1] Implement the code here
-
+		[TASK 1] Implement the code here 
+		
 		1. Write a byte to the buffer incrementing the head index
 		2. If the head index reaches the end of the buf, then reset to zero
 		3. Check if the buf is overrun. We can utilise the data size in the buf
-		   to detect this and maybe discard the last byte.
+		   to detect this and maybe discard the last byte. 
 	*/
-
-	tx_buf[tx_buf_head] = ch;
-
-	tx_buf_head++;
-	tx_data_size++;
-
-	if (tx_buf_head >= BUF_SIZE){
-		tx_buf_head = 0;
+	if (tx_data_size < BUF_SIZE){
+		tx_buf[tx_buf_head] = ch;
+		if (tx_buf_head < (BUF_SIZE - 1)){
+			tx_buf_head++;
+			tx_data_size++;
+		}else{
+			tx_buf_head = 0;
+		}
 	}
-
-	if (tx_data_size >= BUF_SIZE){
-		tx_buf_tail++;
-	}
-
+	
 }
 
 void put_str_buf(unsigned char* str)
 {
-	for (int i=0; i< strlen(str); i++)
+	for (int i=0; i < strlen(str); i++)
 	{
 		put_char_buf(str[i]);
 	}
-
+	
 	// We've put data in the buffer so turn on the TX-UDRE interrupt
-	UCSR0B |= (1<<UDRIE0);
+	UCSR0B |= (1<<UDRIE0);		
 }
 
 void put_char(unsigned char ch)
@@ -99,38 +102,45 @@ void put_char(unsigned char ch)
 	UDR0 = ch;
 }
 
-void get_char(void){
-	
+void put_str(unsigned char* str)
+{
+	while (*str != 0)
+		put_char(*str++);
 }
 
 int main(void)
 {
 	int i = 0;
-	unsigned char str[128];
 
 	DDRB = (1<<DDB1);
-
-	UBRR0H = (unsigned char)(BAUD_VALUE>>8);
+	
+	UBRR0H = (unsigned char)(BAUD_VALUE>>8);	
 	UBRR0L = (unsigned char)(BAUD_VALUE);
 	UCSR0C = (3<<UCSZ00);
-	UCSR0B = (1<<TXEN0) | (1<<RXEN0);
+	UCSR0B = (1<<TXEN0)|(1<<RXEN0)|(1<<RXCIE0);;	
 
+	unsigned char str[128];
 	sei();
+	
+    while (1) 
+    {	
+			if (flag == 1){
+				//put_str("Type keys followed by [enter]...");
+				//sprintf(str,"[%d] Hello World!\r\n",++i);
+		
+				/*
+					[TASK 3] Comment out the polling function put)str()
+					and then demonstrate the working function - put_str_buf() 
+				*/
 
-
-    while (1)
-    {
-
-		// sprintf(str, "[%d] Hello World!\r\n", ++i);
-
-		/*
-			[TASK 3] Comment out the polling function put)str()
-			and then demonstrate the working function - put_str_buf()
-		*/
-		put_str_buf(str);
-		// put_str(str);
-
-		PORTB = ~PORTB;
-		_delay_ms(100);
+				sprintf(str,"[%d][%s]\r\n", ++i, rx_data);
+				memset(rx_data, 0, sizeof(rx_data));
+				put_str_buf(str);
+				PORTB = ~PORTB;
+				rx_buf_head = 0;
+				rx_data_size = 0;
+				flag = 0;
+				_delay_ms(1000);
+			}
     }
 }
